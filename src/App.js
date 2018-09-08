da34c52cd50c0bc39ed911b85f2ef2ca62b4d920
noweverywhere <marinus@noweverywhere.ca>
2018-09-08 23:21:29 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+import MiLibro from './mi_libro/'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <MiLibro />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/mi_libro/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MiLibro extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    return <h1>Hello Mi Libro</h1>
+  }
+}
+
+export default MiLibro
